537febac72b8ee31febe5f753d116e58b60c05ba
Simon MacDonald <simon.macdonald@gmail.com>
2014-02-11 11:26:44 -0500

    Update retrofit jar

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := retrofit:retrofit-1.3.0.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := retrofit:retrofit-1.4.1.jar
 include $(BUILD_MULTI_PREBUILT)
