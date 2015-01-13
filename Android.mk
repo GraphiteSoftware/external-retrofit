40c401d256b20bdb03616d17d0ea777c71f8e438
Andrew Thompson <andrew.thompson@live.ca>
2015-01-13 10:58:43 -0500

    Updating retrofit to 1.9.0.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := retrofit:retrofit-1.4.1.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := retrofit:retrofit-1.9.0.jar
 include $(BUILD_MULTI_PREBUILT)
