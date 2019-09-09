7ad15ba65abcc3027985cf11d570e7cd4b2494ae
oscott-cismobile <50879356+oscott-cismobile@users.noreply.github.com>
2019-09-09 14:00:02 -0400

    G0-799 Upgrade Retrofit library to version 2
    * G0-799 Upgrade Retrofit library to version 2
    
    * Update README.md
    
    * Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := retrofit:retrofit-1.9.0.jar
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    retrofit2:retrofit-2.6.1.jar \
+    gson-converter:converter-gson-2.6.1.jar \
+    okhttp3:okhttp-4.0.1.jar \
+    okhttp3-logging-interceptor:okhttp3-logging-interceptor-4.0.1.jar \
+    kotlin:kotlin-stdlib-1.3.40.jar \
+    kotlinx-coroutines:kotlinx-coroutines-core-1.2.2.jar \
+    okio:okio-2.2.2.jar \
+    annotations:annotations-13.0.jar
+
 include $(BUILD_MULTI_PREBUILT)
--- README.md
+++ README.md
@@ -3,7 +3,4 @@ square:
 
   https://github.com/square/retrofit
 
-If you want to examine the source for release 1.9.0, then
-checkout the "version-one" branch and reset to 2f4caa6f:
-
-  https://github.com/square/retrofit/commit/2f4caa6f
+Source code: https://github.com/square/retrofit/tree/parent-2.6.1
